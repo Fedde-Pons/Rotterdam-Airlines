2645cc5e654cab44fbd4b1317bb963fe9bcf93d8
Fedde-Pons <1116187@hr.nl>
2026-04-07 23:48:13 +0200

    Made some small changes

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -1,26 +1,31 @@
 static class Menu
 {
 
-    // All methods in this class are commented out because they call methods that are not implemented yet. 
-    // You can uncomment them when you have implemented the corresponding methods.
-
     //This shows the menu. You can call back to this method to show the menu again
     //after another presentation method is completed.
     //You could edit this to show different menus depending on the user's role
     static public void Start()
     {
-        Console.WriteLine("Welcome to Rotterdam Airlines!");
+        Console.Clear();
+        Console.WriteLine(@"
+  ____       _   _               _                      _    _      _ _                 
+ |  _ \ ___ | |_| |_ ___ _ __ __| | __ _ _ __ ___      / \  (_)_ __| (_)_ __   ___  ___ 
+ | |_) / _ \| __| __/ _ \ '__/ _` |/ _` | '_ ` _ \    / _ \ | | '__| | | '_ \ / _ \/ __|
+ |  _ < (_) | |_| ||  __/ | | (_| | (_| | | | | | |  / ___ \| | |  | | | | | |  __/\__ \
+ |_| \_\___/ \__|\__\___|_|  \__,_|\__,_|_| |_| |_| /_/   \_\_|_|  |_|_|_| |_|\___||___/
+");
+        Console.WriteLine("Welcome to Rotterdam Airlines!\n");
         Console.WriteLine("1: View all available flights");
         Console.WriteLine("2: Book a flight");
         Console.WriteLine("3: Login/Register");
         Console.WriteLine("4: Exit program");
-        Console.WriteLine("Please enter the number of the option you would like to choose:");
+        Console.WriteLine("\nPlease enter the number of the option you would like to choose:");
 
         string input = Console.ReadLine();
         switch (input)
         {
             case "1":
-                FlightList.ShowAllFlights();
+                FlightList.ShowAllAvailableFlightsList();
                 break;
             case "2":
                 FlightSearch.StartSearch();
@@ -30,11 +35,14 @@ static class Menu
                 AccountMenu();
                 break;
             case "4":
-                Console.WriteLine("Goodbye!");
+                Console.WriteLine("Thank you for using Rotterdam Airlines!\nWe hope to see you again soon!");
                 Environment.Exit(0);
                 break;
             default:
+                Console.Clear();
                 Console.WriteLine("Invalid input, please try again.");
+                Console.WriteLine("Press any key to return to the menu...");
+                Console.ReadKey();
                 Start();
                 break;
         }
@@ -43,18 +51,25 @@ static class Menu
 
     static public void AccountMenu()
     {
+        Console.Clear();
         Console.WriteLine("Would you like to login to your account or register a new account?");
         Console.WriteLine("1: Login");
         Console.WriteLine("2: Register");
-        Console.WriteLine("3: Go back to main menu");
+        Console.WriteLine("3: Go back to the main menu");
+        Console.WriteLine("\nPlease enter the number of the option you would like to choose:");
         string input = Console.ReadLine();
 
         switch (input)
         {
             case "1":
+                Console.Clear();
                 Console.WriteLine("Login functionality is not implemented yet.");
+                Console.WriteLine("Press any key to return to the menu...");
+                Console.ReadKey();
+                AccountMenu();
                 break;
             case "2":
+                Console.Clear();
                 CreateAccountPresent.AccountCreation();
                 Start();
                 break;
@@ -62,7 +77,10 @@ static class Menu
                 Start();
                 break;
             default:
+                Console.Clear();
                 Console.WriteLine("Invalid input, please try again.");
+                Console.WriteLine("Press any key to return to the menu...");
+                Console.ReadKey();
                 AccountMenu();
                 break;
         }
