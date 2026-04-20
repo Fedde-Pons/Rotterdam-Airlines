201368c17089c47f4f84114c3dc2f1be6ed295d3
srhtnl <serhatpp97@gmail.com>
2026-04-21 00:34:25 +0200

    Seating logic WIP will be redon

--- Project/Logic/SeatingLogic.cs
+++ Project/Logic/SeatingLogic.cs
@@ -6,8 +6,6 @@ public static class SeatingLogic
         int totalSeats, 
         int bookedSeats)
     {
-        Console.Clear();
-        Console.WriteLine($"=== SEAT SELECTION FOR FLIGHT {selectedFlight.FlightNumber} ===");
 
         double demandFactor = FactoringLogic.CalculateDemandFactor(bookedSeats, totalSeats);
         DateTime departureDate = DateTime.Parse(selectedFlight.DepartureTime);
@@ -16,50 +14,11 @@ public static class SeatingLogic
         double economyPrice = PricingCoreLogic.CalculateFlightPrice(selectedFlight.BasePrice, demandFactor, timeFactor, "economy");
         double businessPrice = PricingCoreLogic.CalculateFlightPrice(selectedFlight.BasePrice, demandFactor, timeFactor, "business");
 
-        Console.WriteLine("\n--- Business Class ---");
-        foreach (SeatModel seat in availableSeats)
-        {
-            if (seat.Seatclass.Equals("Business", StringComparison.OrdinalIgnoreCase))
-                Console.WriteLine($"- Seat {seat.SeatNumber} | €{businessPrice:F2} | Window: {seat.IsWindows}");
-        }
-
-        Console.WriteLine("\n--- Economy Class ---");
-        foreach (SeatModel seat in availableSeats)
-        {
-            if (seat.Seatclass.Equals("Economy", StringComparison.OrdinalIgnoreCase))
-                Console.WriteLine($"- Seat {seat.SeatNumber} | €{economyPrice:F2} | Window: {seat.IsWindows}");
-        }
-
-        SeatModel chosenSeatModel = null;
-        while (true)
-        {
-            Console.Write("\nEnter the Seat Number you want to book (or type X to cancel): ");
-            string userInput = Console.ReadLine()!.ToUpper();
-
-            if (userInput == "X") 
-            {
-                return null; 
-            }
-
-            foreach (SeatModel seat in availableSeats)
-            {
-                if (seat.SeatNumber.ToUpper() == userInput)
-                {
-                    chosenSeatModel = seat;
-                    break;
-                }
-            }
-
-            if (chosenSeatModel != null) break;
-            else Console.WriteLine("Invalid seat number. Please choose from the list above.");
-        }
+        
 
 
-        double finalPrice = 0;
-        if (chosenSeatModel.Seatclass.Equals("Economy", StringComparison.OrdinalIgnoreCase)) finalPrice = economyPrice;
-        if (chosenSeatModel.Seatclass.Equals("Business", StringComparison.OrdinalIgnoreCase)) finalPrice = businessPrice;
-
-
-        return (chosenSeatModel, finalPrice);
     }
 }
+
+
+// THIS IS STILL IN PROGRESS
